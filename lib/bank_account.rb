98328216fa1627f775e889caafb632523eae8c3f
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-20 03:24:26 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -17,5 +17,14 @@ class BankAccount
     "Your balance is $#{@balance}."
   end
 
+  def valid?
+    if @statue == "open" && @balance > 0
+      true
+    else
+      false
+    end
+  end
+      
+    
 
 end

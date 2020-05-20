b74224930fdedaf1aa4520fea75b1b6c0dda7909
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-20 03:56:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,13 +22,8 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
-      @status = "complete"
-      puts "Transaction was already excuted"
-    elsif
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
-
-      !valid?
     end
 
   end

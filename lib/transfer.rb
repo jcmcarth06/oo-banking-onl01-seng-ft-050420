86247a9073073a007dac218221ec54059c2e89e2
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-20 03:57:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,14 +17,14 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status = "pending" && valid?
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
-    else
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    end
+     if @sender.balance > @amount && @status == "pending" && valid?
+       @sender.balance -= @amount
+       @receiver.balance += @amount
+       @status = "complete"
+     else
+       @status = "rejected"
+       return "Transaction rejected. Please check your account balance."
+     end
   end
 
 

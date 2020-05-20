f5534f182e7446a0976cf30f2a27f1e1eac3f7f4
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-20 03:38:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -8,12 +8,12 @@ class Transfer
     @status = "pending"
   end
 
-  def valid?
-    # if @sender = valid? && @receiver = valid?
-    #   true
-    # else
-    #   false
-    end
-  end
+  # # def valid?
+  # #   # if @sender = valid? && @receiver = valid?
+  # #   #   true
+  # #   # else
+  # #   #   false
+  # #   end
+  # end
 
 end
